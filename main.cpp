b961ad3de341915d9d3f735b741d802d088ab5a0
Theshakyhorse <amaximus.sierra@gmail.com>
2026-04-12 21:13:03 -0700

    Milestone 4

--- main.cpp
+++ main.cpp
@@ -49,22 +49,52 @@ int main() {
 
     cout << endl << "Menu driven part" << endl << endl;
     int choice = Menu();
-    while (choice != 4) {
+    while (choice != 6) {
         if (choice == 1) {
+            int amount;
+            cout << "Enter amount: ";
+            cin >> amount;
+            for (int i = 0; i < amount; i++) {
+                string n;
+                cout << "Villager name: ";
+                cin >> n;
+                int l;
+                cout << "Friendship level: ";
+                cin >> l;
+                while ((l < 0) || (l > 10)) {
+                    cout << "choose a valid friendship level (1-10): ";
+                    cin >> l;
+                }
+                string s;
+                cout << "species: ";
+                cin >> s;
+                string p;
+                cout << "phrase: ";
+                cin >> p;
+                villagerDetails.insert({n, tuple(l, s, p)});
+                cout << n << " " << "added." << endl;
+            }
+        }
+        if (choice == 2) {
+            cout << "enter name of villager below:" << endl;
+            cin >> searchKey;
+            villagerDetails.erase(searchKey);
+        }
+        if (choice == 3) {
             for (auto& [name, details] : villagerDetails) {
                 if (get<0>(details) < 10) {
                     get<0>(details) += 1;
                 }
             }
         }   
-        if (choice == 2) {
+        if (choice == 4) {
             for (auto& [name, details] : villagerDetails) {
                 if (get<0>(details) > 0){
                     get<0>(details) -= 1;
                 }
             }
         }
-        if (choice == 3) {
+        if (choice == 5) {
             cout << "enter name of villager below:" << endl;
             cin >> searchKey;
             // search for an element using .find() to avoid errors
@@ -101,13 +131,15 @@ int main() {
 
 int Menu() {
     int choice;
-    cout << "1. Increase Friendship" << endl;
-    cout << "2. Decrease Friendship" << endl;
-    cout << "3. Search for Villager" << endl;
-    cout << "4. Exit" << endl;
+    cout << "1. Add Villager" << endl;
+    cout << "2. Delete Villager" << endl;
+    cout << "3. Increase Friendship" << endl;
+    cout << "4. Decrease Friendship" << endl;
+    cout << "5. Search for Villager" << endl;
+    cout << "6. Exit" << endl;
     cin >> choice;
     cout << endl;
-    while ((choice < 1) && (choice > 4)){
+    while ((choice < 1) && (choice > 6)){
         cout << "Please choose a valid option" << endl;
         cin >> choice;
     }
